ff2954fcde9f467bf070037ad25963231f115695
Youcef Badaoui <youcefbadaoui@outlook.com>
2023-12-17 00:23:40 +0100

    Revamp
    Completely refactored and improved the code

--- README.md
+++ README.md
@@ -1,16 +1,33 @@
 # Youcef Badaoui
 
-Notes:
-N: is the MAX_SIZE of the buffer and number of consumer threads
+# Variables:
 
-Q1:
+- BUFFER_SIZE: is the size of the buffer, greater than 0 and entered by the user.
+- NUM_CONSUMERS: is the number of consumer threads, greater than 0 and entered by the user.
+- shouldExit: is a boolean variable that indicates whether the consumer threads should exit (when all items are consumed).
+
+# Structures:
+
+- BufferItem: is a structure that represents an item in the buffer, it contains the index of the result matrix (i,j) and the value of the result.
+- BufferQueue: is a structure that represents the buffer, it contains the buffer items, the head and tail indices, and the number of items produced and consumed to keep track of the buffer state.
+
+# Functions:
+
+- insertItem: is a function that inserts an item in the buffer, it takes the value of the result and the index of the result matrix (i,j) as parameters. (Used by the producer threads)
+- removeItem: is a function that removes an item from the buffer, it returns the value of the result and the index of the result matrix (i,j). (Used by the consumer threads)
+- Helper functions: are functions that are used to send helpful debug messages to the console. and generate, print and clear matrices.
+
+# Q1:
+
 We use a two-dimensional array to represent the matrices A, B and C.
-and a circular queue to represent the buffer T, (arrays of size N for the intermediate results and their indices.)
+and a circular queue to represent the buffer T (BufferQueue and the items inside it as BufferItem).
 
-Q2:
+# Q2:
+
 Each producer thread must process a distinct section of matrix B and C. Thus, data sharing is done by dividing the tasks.
-We also use semaphores to protect and synchronize access to shared data (result matrix and buffer)
+We also use semaphores to protect and synchronize access to shared data (result matrix and buffer) and lock the critical sections.
+And suspend the producer threads when the buffer is full and the consumer threads when the buffer is empty.
 
-Q3:
-The only risk i could have think of in my implemntation is Unpredictable results due to the use of the same buffer by all threads in case an unexpected error or overflow occurs.
-Also possible lack of error handling in case of unexpected errors or inputs.
+# Q3:
+
+Current implementation does not have any predictable risks, because we are using semaphores to carefully protect and synchronize access to shared data (result matrix and buffer) and lock the critical sections.
--- matrice.c
+++ matrice.c
@@ -1,85 +1,133 @@
 /*
  * Author:   Youcef BADAOUI
  * Email:    youcefbadaoui@outlook.com
- * Date:     2023/12/15
+ * Date:     2023/12/16
  */
 
-// Includes
+/* Required headers */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <sys/time.h>
 
-// Global variables
-int n1, m1, n2, m2;
-int **A;
-int **B;
-int **C;
-int shouldExit = 0;
+/* Helper Prototypes */
+void DEBUG(const char *threadType, int threadIndex, const char *msg, int *item);
+void GENERATE_MATRIX(int ***globalMatrix, int n, int m, const char *name);
+void PRINT_MATRIX(int **matrix, int n, int m, const char *name);
+void CLEAR_MATRIX(int ***matrix, int n);
 
-// Buffer
-#define N 5 // Max number of items in buffer
-typedef struct T
+/* Buffer Item */
+typedef struct BufferItem
 {
-    int *data;
-    int *posX;
-    int *posY;
-    int count;
-    int in;
-    int out;
-    int produced;
-    int consumed;
-} T;
-T buffer;
+    int value; // Value of item (result of multiplication)
+    int i;     // Row index in matrix A
+    int j;     // Column index in matrix A
+} BufferItem;
 
-// Semaphores
-pthread_mutex_t mutex;
-sem_t empty;
-sem_t full;
+/* Buffer Queue (Circular) */
+typedef struct BufferQueue
+{
+    BufferItem *items; // Array of items T[BUFFER_SIZE]
+    int inIndex;       // Index of next item to insert
+    int outIndex;      // Index of next item to remove
+    int produced;      // Number of items produced
+    int consumed;      // Number of items consumed
+} BufferQueue;
 
-// Producer
+/* Global variables */
+int BUFFER_SIZE, NUM_CONSUMERS; // Buffer size and number of consumers threads
+int n1, m1, n2, m2;             // Dimensions of matrices
+int **A, **B, **C;              // Matrices
+int shouldExit = 0;             // Flag to exit
+BufferQueue buffer;             // Buffer queue
+
+/* Semaphores */
+pthread_mutex_t mutex; // Mutex for critical section
+sem_t empty;           // Flag to indicate if buffer is empty
+sem_t full;            // Flag to indicate if buffer is full
+
+/* Insert Item Function */
+void insertItem(int value, int i, int j)
+{
+    // Produce item
+    BufferItem item;
+    item.value = value;
+    item.i = i;
+    item.j = j;
+
+    // Insert item in buffer
+    buffer.items[buffer.inIndex] = item;
+    buffer.inIndex = (buffer.inIndex + 1) % BUFFER_SIZE;
+    buffer.produced++;
+}
+
+/* Remove Item Function */
+BufferItem removeItem()
+{
+    // Consume item from buffer
+    BufferItem item = buffer.items[buffer.outIndex];
+    buffer.outIndex = (buffer.outIndex + 1) % BUFFER_SIZE;
+    buffer.consumed++;
+
+    return item;
+}
+
+/* Producer Thread */
 void *producer(void *arg)
 {
-    int index = *(int *)arg;
-    int item = 0;
+    int index = *(int *)arg; // Producer index (Used to identify thread and what row to calculate)
+    int sum = 0;             // Sum of multiplication
+
+    /*** DEBUG ***/
+    DEBUG("Producer", index + 1, "STARTED.", NULL);
+    /*** DEBUG ***/
 
     for (int i = 0; i < m2; i++)
     {
-        item = 0;
+        // Calculate item
+        sum = 0;
         for (int j = 0; j < m1; j++)
-            item += B[index][j] * C[j][i];
+            sum += B[index][j] * C[j][i];
 
-        sem_wait(&empty);
-        pthread_mutex_lock(&mutex);
+        sem_wait(&empty);           // If buffer is full, wait
+        pthread_mutex_lock(&mutex); // Lock critical section
 
-        buffer.data[buffer.in] = item;
-        buffer.posX[buffer.in] = index;
-        buffer.posY[buffer.in] = i;
-        buffer.in = (buffer.in + 1) % N;
-        buffer.count++;
-        buffer.produced++;
+        // Insert result in buffer
+        insertItem(sum, index, i);
 
-        // printf("Producer [%d] Produced item: %d\n", index + 1, item);
+        /*** DEBUG ***/
+        DEBUG("Producer", index + 1, "Produced item", &sum);
+        /*** DEBUG ***/
 
-        pthread_mutex_unlock(&mutex);
-        sem_post(&full);
+        pthread_mutex_unlock(&mutex); // Unlock critical section
+        sem_post(&full);              // Signal that buffer is full
     }
 
-    // printf("**Producer [%d] Finished.\n", index + 1);
+    /*** DEBUG ***/
+    DEBUG("Producer", index + 1, "FINISHED.", NULL);
+    /*** DEBUG ***/
+
+    // Free allocated memory and exit thread
+    free(arg);
     pthread_exit(NULL);
 }
 
-// Consumer
+/* Consumer Thread */
 void *consumer(void *arg)
 {
-    int index = *(int *)arg;
-    int item = 0;
+    int index = *(int *)arg; // Consumer index (Used to identify thread)
 
+    /*** DEBUG ***/
+    DEBUG("Consumer", index + 1, "STARTED.", NULL);
+    /*** DEBUG ***/
+
+    // Consume items from buffer until all items are consumed
     while (!shouldExit)
     {
-        sem_wait(&full);
-        pthread_mutex_lock(&mutex);
+        sem_wait(&full);            // If buffer is empty, wait
+        pthread_mutex_lock(&mutex); // Lock critical section
 
         if (shouldExit)
         {
@@ -87,84 +135,73 @@ void *consumer(void *arg)
             break;
         }
 
-        item = buffer.data[buffer.out];
-        A[buffer.posX[buffer.out]][buffer.posY[buffer.out]] = item;
-        buffer.out = (buffer.out + 1) % N;
-        buffer.count--;
-        buffer.consumed++;
+        // Get item from buffer
+        BufferItem item = removeItem();
 
-        // printf("Consumer [%d] Consumed item: %d\n", index + 1, item);
+        // Insert item in matrix A
+        A[item.i][item.j] = item.value;
 
+        /*** DEBUG ***/
+        DEBUG("Consumer", index + 1, "Consumed", &item.value);
+        /*** DEBUG ***/
+
+        // Check if all items are consumed
         shouldExit = buffer.consumed == n1 * m2;
         if (shouldExit)
-            for (int i = 0; i < N; i++)
-                sem_post(&full);
+            for (int i = 0; i < NUM_CONSUMERS; i++)
+                sem_post(&full); // Signal all consumers to exit
 
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
+        pthread_mutex_unlock(&mutex); // Unlock critical section
+        sem_post(&empty);             // Signal that buffer is empty
     }
 
-    // printf("**Consumer [%d] Finished.\n", index + 1);
+    /*** DEBUG ***/
+    DEBUG("Consumer", index + 1, "FINISHED.", NULL);
+    /*** DEBUG ***/
+
+    // Free allocated memory and exit thread
+    free(arg);
     pthread_exit(NULL);
 }
 
-// Generate random matrices
-void generatorMatrices(void)
-{
-    // Generate matrix B
-    B = (int **)malloc(n1 * sizeof(int *));
-    for (int i = 0; i < n1; i++)
-        B[i] = (int *)malloc(m1 * sizeof(int));
-
-    if (B == NULL)
-        perror("Memory allocation for matric B failed.\n");
-
-    // Generate matrix C
-    C = (int **)malloc(n2 * sizeof(int *));
-    for (int i = 0; i < n2; i++)
-        C[i] = (int *)malloc(m2 * sizeof(int));
-
-    if (C == NULL)
-        perror("Memory allocation for matric C failed.\n");
-
-    // Fill matrix B with random numbers
-    for (int i = 0; i < n1; i++)
-        for (int j = 0; j < m1; j++)
-            B[i][j] = rand() % 10;
-
-    // Fill matrix C with random numbers
-    for (int i = 0; i < n2; i++)
-        for (int j = 0; j < m2; j++)
-            C[i][j] = rand() % 10;
-
-    // Pretty print both matrices
-    printf("\n| Matrix B\n");
-    for (int i = 0; i < n1; i++)
-    {
-        for (int j = 0; j < m1; j++)
-            printf("%d  ", B[i][j]);
-        printf("\n");
-    }
-    printf("\n| Matrix C\n");
-    for (int i = 0; i < n2; i++)
-    {
-        for (int j = 0; j < m2; j++)
-            printf("%d  ", C[i][j]);
-        printf("\n");
-    }
-    printf("\n\n");
-}
-
+/* Program Start */
 int main(int argc, char *argv[])
 {
-    // Seed the random number generator
+    // Seed random number generator (Or else it will generate the same numbers every time)
     srand(time(NULL));
 
-    // Ask user for n1,m1 for B[n1][m1] and n2,m2 for C[n2][m2]
+    // Ask user for buffer size and number of consumers
+    printf("Enter buffer size: ");
+    scanf("%d", &BUFFER_SIZE);
+    printf("Enter number of consumers: ");
+    scanf("%d", &NUM_CONSUMERS);
+
+    // Make sure that buffer size and number of consumers are greater than 0
+    if (BUFFER_SIZE <= 0 || NUM_CONSUMERS <= 0)
+    {
+        perror("Buffer size and number of consumers must be greater than 0.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Init buffer
+    buffer.items = (BufferItem *)malloc(BUFFER_SIZE * sizeof(BufferItem));
+    buffer.inIndex = 0;
+    buffer.outIndex = 0;
+    buffer.produced = 0;
+    buffer.consumed = 0;
+
+    // Ask for dimensions of matrices
     printf("Enter number of rows and cols in matrix B: ");
     scanf("%d %d", &n1, &m1);
     printf("Enter number of rows and cols in matrix C: ");
     scanf("%d %d", &n2, &m2);
+
+    // Validate that demensions can form a matrix
+    if (n1 <= 0 || m1 <= 0 || n2 <= 0 || m2 <= 0)
+    {
+        perror("Dimensions of matrices must be greater than 0.\n");
+        exit(EXIT_FAILURE);
+    }
 
     // Make sure that multiplication is possible
     if (m1 != n2)
@@ -173,17 +210,9 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // Generate matrices
-    generatorMatrices();
-
-    buffer.data = (int *)malloc(N * sizeof(int));
-    buffer.posX = (int *)malloc(N * sizeof(int));
-    buffer.posY = (int *)malloc(N * sizeof(int));
-    buffer.count = 0;
-    buffer.in = 0;
-    buffer.out = 0;
-    buffer.produced = 0;
-    buffer.consumed = 0;
+    // Generate matrices B and C
+    GENERATE_MATRIX(&B, n1, m1, "B");
+    GENERATE_MATRIX(&C, n2, m2, "C");
 
     // Init matrix A
     A = (int **)malloc(n1 * sizeof(int *));
@@ -200,7 +229,7 @@ int main(int argc, char *argv[])
         perror("Mutex init failed.\n");
         exit(EXIT_FAILURE);
     }
-    if (sem_init(&empty, 0, N) != 0)
+    if (sem_init(&empty, 0, BUFFER_SIZE) != 0)
     {
         perror("Semaphore [empty] init failed.\n");
         exit(EXIT_FAILURE);
@@ -211,9 +240,9 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // Declare threads
+    // Declare threads arrays
     pthread_t producers_t[n1];
-    pthread_t consumer_t[N];
+    pthread_t consumer_t[NUM_CONSUMERS];
 
     // Create threads
     for (int i = 0; i < n1; i++)
@@ -227,7 +256,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < NUM_CONSUMERS; i++)
     {
         int *index = malloc(sizeof(int));
         *index = i;
@@ -238,44 +267,109 @@ int main(int argc, char *argv[])
         }
     }
 
-    // Join threads
+    // Wait for threads to finish
     for (int i = 0; i < n1; i++)
         pthread_join(producers_t[i], NULL);
 
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < NUM_CONSUMERS; i++)
         pthread_join(consumer_t[i], NULL);
 
-    // Pretty print matrix A
-    printf("\n| Matrix A\n");
-    for (int i = 0; i < n1; i++)
-    {
-        for (int j = 0; j < m2; j++)
-            printf("%d  ", A[i][j]);
-        printf("\n");
-    }
+    // Print matrix A (Result)
+    PRINT_MATRIX(A, n1, m2, "A");
 
-    // Cleanup
+    // Cleanup and exit
     pthread_mutex_destroy(&mutex);
     sem_destroy(&empty);
     sem_destroy(&full);
-    for (int i = 0; i < n1; i++)
-    {
-        free(A[i]);
-    }
-    free(A);
-    for (int i = 0; i < n1; i++)
-    {
-        free(B[i]);
-    }
-    free(B);
-    for (int i = 0; i < n1; i++)
-    {
-        free(C[i]);
-    }
-    free(C);
-    free(buffer.data);
-    free(buffer.posX);
-    free(buffer.posY);
+    CLEAR_MATRIX(&A, n1);
+    CLEAR_MATRIX(&B, n1);
+    CLEAR_MATRIX(&C, n2);
 
     return 0;
+}
+
+/* Helper Functions */
+void DEBUG(const char *threadType, int threadIndex, const char *msg, int *item)
+{
+    // This function should print the message in this format: [HH:MM:SS:MS][THREAD]: message
+    // Where HH:MM:SS is the current time, THREAD is the name of the thread and message is the message itself
+    // Example: [12:30:15:59][Consumer #1]: consumed item: 5
+    // Helpful for debugging and understanding what is happening
+
+    // Get current time
+    struct timeval currentTime;
+    gettimeofday(&currentTime, NULL);
+
+    // Convert time to struct tm
+    time_t now = currentTime.tv_sec;
+    struct tm *t = localtime(&now);
+
+    // Get milliseconds
+    long milliseconds = currentTime.tv_usec / 1000;
+
+    // Print message
+    if (item == NULL)
+        printf("[%02d:%02d:%02d:%03ld][%s #%d]: %s\n", t->tm_hour, t->tm_min, t->tm_sec, milliseconds, threadType, threadIndex, msg);
+    else
+        printf("[%02d:%02d:%02d:%03ld][%s #%d]: %s: %d\n", t->tm_hour, t->tm_min, t->tm_sec, milliseconds, threadType, threadIndex, msg, *item);
+}
+
+void PRINT_MATRIX(int **matrix, int n, int m, const char *name)
+{
+    printf("\n| Matrix %s\n", name);
+    // Find the maximum number of digits in the matrix
+    int maxDigits = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int num = matrix[i][j];
+            int digits = 1;
+            while (num /= 10)
+            {
+                digits++;
+            }
+            if (digits > maxDigits)
+            {
+                maxDigits = digits;
+            }
+        }
+    }
+
+    // Print the matrix with organized spacing
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%*d ", maxDigits, matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void GENERATE_MATRIX(int ***matrix, int n, int m, const char *name)
+{
+    // Generate matrix
+    *matrix = (int **)malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++)
+        (*matrix)[i] = (int *)malloc(m * sizeof(int));
+
+    if (matrix == NULL)
+        perror("Memory allocation for matric failed.\n");
+
+    // Fill matrix with random numbers
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            (*matrix)[i][j] = rand() % 10;
+
+    // Pretty print matrix
+    PRINT_MATRIX(*matrix, n, m, name);
+}
+
+void CLEAR_MATRIX(int ***matrix, int n)
+{
+    for (int i = 0; i < n; i++)
+        free((*matrix)[i]);
+    free(*matrix);
 }
