5b2ac9970855acf3be3c16e84079c0bfe378c68f
imane bouduettaia <130753979+imane1904@users.noreply.github.com>
2023-12-25 10:02:09 +0100

    Update matrice.c
    In the extended description, let's delve deeper into the code and its components:
    
    Code Structure:
    Matrices B, C, et A:
    B (n1 x m1): La première matrice.
    C (m1 x n2): La deuxième matrice.
    A (n1 x n2): La matrice résultante.
    Tampon T:
    T (N): Un tampon pour stocker temporairement les résultats intermédiaires.
    Synchronisation:
    mutex: Un verrou pour assurer l'exclusion mutuelle lors de l'accès aux données partagées.
    empty: Un sémaphore pour suivre le nombre de places libres dans le tampon.
    full: Un sémaphore pour suivre le nombre de places remplies dans le tampon.
    Producteur:
    La fonction du producteur est d'itérer sur chaque ligne de la matrice résultante A.
    Elle calcule la ligne correspondante en effectuant le produit matriciel de la ligne de B et de la matrice C.
    Les résultats intermédiaires sont temporairement stockés dans le tampon T.
    Chaque producteur attend qu'une place libre soit disponible dans le tampon avant d'y insérer le résultat.
    L'utilisation d'un verrou (mutex) garantit une insertion sûre dans le tampon.
    Consommateur:
    La fonction du consommateur est d'extraire les résultats intermédiaires du tampon T.
    Elle place chaque ligne résultante dans la matrice A au bon emplacement.
    Chaque consommateur attend qu'une place remplie soit disponible dans le tampon avant de récupérer le résultat.
    L'utilisation du verrou (mutex) garantit une extraction sûre du tampon.
    Main:
    Initialisation des matrices B et C avec des valeurs aléatoires.
    Initialisation des sémaphores et du verrou.
    Création des threads producteurs et consommateurs.
    Attente des threads producteurs.
    Indication aux consommateurs que la production est terminée.
    Attente des threads consommateurs.
    Destruction des ressources.
    Risques à Considérer:
    Conditions de concurrence: Des problèmes peuvent survenir si plusieurs threads tentent d'accéder et de modifier les mêmes données simultanément sans synchronisation appropriée.
    Blocages: Un mauvais placement de sémaphores peut entraîner des blocages où les threads ne peuvent pas progresser

--- matrice.c
+++ matrice.c
@@ -1,17 +1,19 @@
-//section include..
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
 
+#define N ... // Taille du tampon
+#define n1 ... // Nombre de lignes de B
+#define m1 ... // Nombre de colonnes de B (également nombre de lignes de C)
+#define n2 ... // Nombre de colonnes de C
 
+// Matrices
+double B[n1][m1], C[m1][n2], A[n1][n2];
+// Tampon
+double T[N];
 
-//define
-#define N ... // places dans le buffer
-
-//variable globales 
-//les matrices
-B,C,A
-//le tampon
-T
-
-//pour la synchronisation 
+// Synchronisation
 pthread_mutex_t mutex;
 sem_t empty;
 sem_t full;
@@ -19,52 +21,112 @@ sem_t full;
 // Producteur
 void producer(void)
 {
-  int item;
-//pour chaque ligne 
-//for....
-  {
-    item=produce(item);
-    sem_wait(&empty); // attente d'une place libre
-    pthread_mutex_lock(&mutex);
-     // section critique
-     insert_item();
-    pthread_mutex_unlock(&mutex);
-    sem_post(&full); // il y a une place remplie en plus
-  }
+    int row;
+    while (1)
+    {
+        // Obtenir la prochaine ligne à calculer
+        pthread_mutex_lock(&mutex);
+        row = getNextRow();
+        pthread_mutex_unlock(&mutex);
+
+        // Vérifier la condition de terminaison
+        if (row == -1)
+            break;
+
+        // Calculer la ligne de la matrice résultante A et la stocker dans le tampon T
+        for (int col = 0; col < n2; ++col)
+        {
+            double result = 0.0;
+            for (int i = 0; i < m1; ++i)
+            {
+                result += B[row][i] * C[i][col];
+            }
+            T[col] = result;
+        }
+
+        sem_wait(&empty); // attente d'une place libre
+        pthread_mutex_lock(&mutex);
+        // section critique
+        insert_item(row, T);
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full); // il y a une place remplie en plus
+    }
 }
 
+// Consommateur
 void consumer(void)
 {
- int item;
- while(true)
- {
-   sem_wait(&full); // attente d'une place remplie
-   pthread_mutex_lock(&mutex);
-    // section critique
-    item=remove(item);
-   pthread_mutex_unlock(&mutex);
-   sem_post(&empty); // il y a une place libre en plus
- }
+    int row;
+    double resultRow[n2];
+
+    while (1)
+    {
+        sem_wait(&full); // attente d'une place remplie
+        pthread_mutex_lock(&mutex);
+        // section critique
+        row = remove_item(resultRow);
+        pthread_mutex_unlock(&mutex);
+        sem_post(&empty); // il y a une place libre en plus
+
+        // Placer la ligne résultante dans la matrice résultante A
+        for (int col = 0; col < n2; ++col)
+        {
+            A[row][col] = resultRow[col];
+        }
+
+        // Vérifier la condition de terminaison et quitter si nécessaire
+        if (row == -1)
+            break;
+    }
 }
 
-int Main ()
+int main()
 {
-// Initialisation
-sem_init(&mutex,0,1);//exclusion mutuelle 
-sem_init(&empty, 0 , N);  // buffer vide
-sem_init(&full, 0 , 0);   // buffer vide
-//creation des threads
+    // Initialisation des matrices B et C avec des valeurs aléatoires
+    // ...
 
+    // Initialisation
+    sem_init(&empty, 0, N);  // buffer vide
+    sem_init(&full, 0, 0);   // buffer vide
+    pthread_mutex_init(&mutex, NULL); // exclusion mutuelle
 
+    // Création des threads producteurs
+    pthread_t producers[n1];
+    for (int i = 0; i < n1; ++i)
+    {
+        pthread_create(&producers[i], NULL, producer, NULL);
+    }
 
-//attente des threads
+    // Création des threads consommateurs
+    pthread_t consumers[n1];
+    for (int i = 0; i < n1; ++i)
+    {
+        pthread_create(&consumers[i], NULL, consumer, NULL);
+    }
 
+    // Attente des threads producteurs
+    for (int i = 0; i < n1; ++i)
+    {
+        pthread_join(producers[i], NULL);
+    }
 
+    // Indiquer aux consommateurs que la production est terminée
+    for (int i = 0; i < n1; ++i)
+    {
+        sem_post(&full);
+    }
 
+    // Attente des threads consommateurs
+    for (int i = 0; i < n1; ++i)
+    {
+        pthread_join(consumers[i], NULL);
+    }
 
-//destruction...
+    // Destruction des ressources
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    pthread_mutex_destroy(&mutex);
 
-
-
-return 0;
+    return 0;
 }
+
