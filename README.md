f8b27c1df466ccc413aedcfbddcb18600bfd7192
khelif-Abdellah&Sami-Allouche <aimankhelif1@gmail.com>
2023-12-31 18:17:02 +0100

    projet-Se
    khelif Abdallah & Sami Allouche

--- README.md
+++ README.md
@@ -1,30 +1,26 @@
-# projet
-projet système d'exploitation
+Q1: Quelles structures de données devraient être utilisées ?
 
-On veut effectuer en parallèle(En utilisant le modèle producteurs/consommateur) le produit de deux matrices: 
-B (n1* m1)  et C (n2 * m2) ⇒ la matrice résultante A=B*C ;
+-Matrices B, C et A :
+Celles-ci peuvent être représentées sous forme de tableaux 2D ou de matrices.
 
-Les matrices sont remplis par des valeurs aléatoires
+-Buffer T :
+Un tampon circulaire ou une structure de file d'attente peut être utilisé pour implémenter le tampon T. Ce tampon contiendra les résultats intermédiaires produits par les threads producteurs.
 
-Les résultats intermédiaires seront placés dans un tampon de taille “T[N]”.
+Q2: Comment protégerez-vous l'accès à ces données ?
 
-Chaque threads producteurs calcule une ligne de la matrice résultante A et range les résultat dans le tampon T
+-Mutex (Exclusion Mutuelle) :
+Utilisez des verrous mutex pour garantir qu'un seul thread peut accéder aux données partagées (par exemple, aux matrices, au tampon) à la fois. Cela empêche les conditions de concurrence et la corruption des données.
 
-Les threads consommateurs consomment l'élément T[y]  le place dans la matrice résultante A  au bon emplacement!
+-Sémaphore :
+Des sémaphores peuvent être utilisés pour contrôler l'accès au tampon T. Les sémaphores peuvent signaler quand le tampon est disponible pour l'écriture ou la lecture.
 
-q1: Quelles sont les structures de données à utiliser ?
+Q3: Quels sont les risques ?
 
-q2: Comment allez-vous protéger l'accès à ces données?
+-Conditions de Course :
+Sans une synchronisation adéquate, des conditions de course peuvent survenir, entraînant des résultats incorrects ou une corruption des données.
 
-q3- quels sont les risques?
+-Interblocages :
+Une utilisation inefficace des mécanismes de synchronisation peut conduire à des interblocages, où les threads restent bloqués indéfiniment en attendant les uns les autres.
 
-1-Cloner le projet github : projet  ; et le modifier le selon les exigences ci-dessus
-
-2- Pour chaque nouvelle idée créer une nouvelle branche; les autres étudiants peuvent améliorer l'idée en créant une nouvelle branche!
-
-3-Les premières réponses sont mieux notées!
-
-4-Bien gérer les éxceptions 
-
-5-Bien gérer les messages d'erreurs!
-
+-Incohérence des Données :
+Si la synchronisation n'est pas correcte, un accès concurrent aux données partagées peut entraîner des données incorrectes ou incohérentes.
--- matrice.c
+++ matrice.c
@@ -1,70 +1,131 @@
-//section include..
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
 
+#define N1 3
+#define M1 4
+#define N2 4
+#define M2 3
+#define BUFFER_SIZE 5
 
+int B[N1][M1];
+int C[N2][M2];
+int A[N1][M2];
+int buffer[BUFFER_SIZE][M2];
+int in = 0, out = 0;
 
-//define
-#define N ... // places dans le buffer
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t buffer_full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t buffer_empty = PTHREAD_COND_INITIALIZER;
 
-//variable globales 
-//les matrices
-B,C,A
-//le tampon
-T
+void initialize_matrices() {
+    
+    srand(time(NULL));
 
-//pour la synchronisation 
-pthread_mutex_t mutex;
-sem_t empty;
-sem_t full;
+    for (int i = 0; i < N1; ++i) {
+        for (int j = 0; j < M1; ++j) {
+            B[i][j] = rand() % 10;
+        }
+    }
 
-// Producteur
-void producer(void)
-{
-  int item;
-//pour chaque ligne 
-//for....
-  {
-    item=produce(item);
-    sem_wait(&empty); // attente d'une place libre
-    pthread_mutex_lock(&mutex);
-     // section critique
-     insert_item();
-    pthread_mutex_unlock(&mutex);
-    sem_post(&full); // il y a une place remplie en plus
-  }
+    for (int i = 0; i < N2; ++i) {
+        for (int j = 0; j < M2; ++j) {
+            C[i][j] = rand() % 10;
+        }
+    }
 }
 
-void consumer(void)
-{
- int item;
- while(true)
- {
-   sem_wait(&full); // attente d'une place remplie
-   pthread_mutex_lock(&mutex);
-    // section critique
-    item=remove(item);
-   pthread_mutex_unlock(&mutex);
-   sem_post(&empty); // il y a une place libre en plus
- }
+void print_matrix(int rows, int cols, int matrix[rows][cols]) {
+   
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
-int Main ()
-{
-// Initialisation
-sem_init(&mutex,0,1);//exclusion mutuelle 
-sem_init(&empty, 0 , N);  // buffer vide
-sem_init(&full, 0 , 0);   // buffer vide
-//creation des threads
+void* producer(void* arg) {
+    int thread_id = *(int*)arg;
+    for (int i = 0; i < N1; ++i) {
+        sleep(1);
+        for (int j = 0; j < M2; ++j) {
+            A[i][j] = 0;
+            for (int k = 0; k < M1; ++k) {
+                A[i][j] += B[i][k] * C[k][j];
+            }
+        }
 
+        pthread_mutex_lock(&mutex);
+        while ((in + 1) % BUFFER_SIZE == out) {
+            printf("Producer %d: Buffer full, waiting...\n", thread_id);
+            pthread_cond_wait(&buffer_empty, &mutex);
+        }
+  
+        for (int j = 0; j < M2; ++j) {
+            buffer[in][j] = A[i][j];
+        }
 
+        printf("Producer %d: Produced row %d\n", thread_id, i);
+        in = (in + 1) % BUFFER_SIZE;
+        pthread_cond_signal(&buffer_full);
+        pthread_mutex_unlock(&mutex);
+    }
 
-//attente des threads
+    pthread_exit(NULL);
+}
 
+void* consumer(void* arg) {
+    int thread_id = *(int*)arg;
+    for (int i = 0; i < N1; ++i) {
+        pthread_mutex_lock(&mutex);
+        while (in == out) {
+            printf("Consumer %d: Buffer empty, waiting...\n", thread_id);
+            pthread_cond_wait(&buffer_full, &mutex);
+        }
 
+        for (int j = 0; j < M2; ++j) {
+            A[i][j] = buffer[out][j];
+        }
 
+        printf("Consumer %d: Consumed row %d\n", thread_id, i);
+        out = (out + 1) % BUFFER_SIZE;
+        pthread_cond_signal(&buffer_empty);
+        pthread_mutex_unlock(&mutex);
+    }
 
-//destruction...
+    pthread_exit(NULL);
+}
 
+int main() {
+    initialize_matrices();
+   
+    printf("Matrix B:\n");
+    print_matrix(N1, M1, B);
+    printf("Matrix C:\n");
+    print_matrix(N2, M2, C);
 
+    pthread_t producer_thread, consumer_thread;
+    int producer_id = 1, consumer_id = 2; 
 
-return 0;
+    if (pthread_create(&producer_thread, NULL, producer, &producer_id) != 0) {
+        perror("Error creating producer thread");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_create(&consumer_thread, NULL, consumer, &consumer_id) != 0) {
+        perror("Error creating consumer thread");
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_join(producer_thread, NULL);
+    pthread_join(consumer_thread, NULL);
+
+    printf("Resulting Matrix A:\n");
+    print_matrix(N1, M2, A);
+
+    return 0;
 }
