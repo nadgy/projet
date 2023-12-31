dd4df4393b0a5e75122a7140c73000be7cd1588f
abdelmadjid-larik <155304466+abdelmadjid-larik@users.noreply.github.com>
2023-12-31 22:52:47 +0100

    Update matrice.c

--- matrice.c
+++ matrice.c
@@ -1,70 +1,180 @@
-//section include..
+// Noui Mehdi G02
+// Abdelmadjid Larik G02
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#define N 100 
 
-
-//define
-#define N ... // places dans le buffer
-
-//variable globales 
-//les matrices
-B,C,A
-//le tampon
-T
-
-//pour la synchronisation 
+int L1,C1,L2,C2;
+int **A, **B, **C;
+int *BUFFER;
 pthread_mutex_t mutex;
-sem_t empty;
-sem_t full;
+sem_t empty,full;
 
-// Producteur
-void producer(void)
-{
-  int item;
-//pour chaque ligne 
-//for....
-  {
-    item=produce(item);
-    sem_wait(&empty); // attente d'une place libre
-    pthread_mutex_lock(&mutex);
-     // section critique
-     insert_item();
-    pthread_mutex_unlock(&mutex);
-    sem_post(&full); // il y a une place remplie en plus
-  }
-}
-
-void consumer(void)
-{
- int item;
- while(true)
- {
-   sem_wait(&full); // attente d'une place remplie
-   pthread_mutex_lock(&mutex);
-    // section critique
-    item=remove(item);
-   pthread_mutex_unlock(&mutex);
-   sem_post(&empty); // il y a une place libre en plus
+void Matrices_Generator(){
+     //memory allocation of the matrices
+     int i,j;
+     B=(int**)malloc(L1*sizeof(int*)); 
+     C=(int**)malloc(C1*sizeof(int*));
+     for(i=0;i<C1;i++){ 
+         B[i]=(int*)malloc(C1*sizeof(int)); 
+         C[i]=(int*)malloc(C2*sizeof(int)); 
+     } 
+     //generates a random set of numbers for the matrices
+     //For B
+     for (i=0;i<L1;i++){ 
+         for(j=0;j<C1;j++){ 
+             B[i][j]=rand()%10; 
+         } 
+     } 
+     //For C
+     for(i=0;i<L2;i++) { 
+         for(j=0;j<C2;j++) { 
+             C[i][j]=rand()%10; 
+         } 
+     } 
+     //set the precise size for the buffer
+     BUFFER=(int*)malloc(L1*C2*sizeof(int)); 
  }
+ void Matrices_Printer(){
+     int i,j; 
+     //print Matrix B
+     printf("\nMatrix B:\n"); 
+     for(i=0;i<L1;i++){ 
+         for (j=0;j<C1;j++){ 
+             printf("%d\t",B[i][j]); 
+         } 
+         printf("\n"); 
+     } 
+     //print Matrix C
+     printf("\nMatrix C:\n"); 
+     for(i=0;i<L2;i++){
+         for(j=0;j<C2;j++){ 
+             printf("%d\t",C[i][j]); 
+         } 
+         printf("\n"); 
+     } 
+     printf("\n"); 
+ } 
+
+void Matrices_Destroyer(){
+     int i,j;
+     //free the allocated memoty of the matrix B
+     for(i=0;i<L1;i++)free(B[i]);free(B);
+     //free the allocated memory of the matrix C
+     for(i=0;i<C1;i++)free(C[i]);free(C);
+     //free the allocated memory of the buffer
+     free(BUFFER);
 }
 
-int Main ()
-{
-// Initialisation
-sem_init(&mutex,0,1);//exclusion mutuelle 
-sem_init(&empty, 0 , N);  // buffer vide
-sem_init(&full, 0 , 0);   // buffer vide
-//creation des threads
+void *producer(void *args){
+     int row=*(int *)args,temp;
+     int i,j;
+     for (i=0;i<C2;i++){
+	 temp=0;
+	 //calculate the row's elements
+         for(j=0;j<C1;j++){
+             temp+=B[row][j]*C[j][i];
+         }
+	 sem_wait(&empty);
+         pthread_mutex_lock(&mutex);
+         BUFFER[row*C2+i]=temp;
+         pthread_mutex_unlock(&mutex);
+         sem_post(&full);
+     }
+    pthread_exit(NULL);
+ }
 
+ void *consumer(void *args){
+     int row=*(int*)args;
+     int i,j;
+     for (i=0;i<C2;i++){
+         sem_wait(&full);
+         pthread_mutex_lock(&mutex);
+         A[row][i]=BUFFER[(row*C2+i)%(L1*C2)];
+         pthread_mutex_unlock(&mutex);
+         sem_post(&empty);
+     }
+     pthread_exit(NULL);
+ }
+ 
+ int main(int argc, char *argv[]) 
+ { 
+     sem_init(&full,0,0); 
+     sem_init(&empty,0,N); 
+     pthread_mutex_init(&mutex, NULL); 
+  	
+     //SETTING THE MATRICES UP  
+     printf("Set the dimensions of Matrix B:\n"); 
+     printf("Set the number of rows :");
+     scanf("%d",&L1);
+     printf("set the number of Columns :");
+     scanf("%d",&C1);
 
+     printf("Set the dimensions of Matrix C:\n"); 
+     printf("Set the number of rows :");
+     scanf("%d",&L2);
+     printf("set the number of Columns :");
+     scanf("%d",&C2);
+     printf("\n");
+     
+     if(C1!=L2){ 
+         perror("Failure.\nMatrix dimensions do not match for a multipicaiton\n"); 
+         exit(EXIT_FAILURE); 
+     }
+     Matrices_Generator(); 
+     Matrices_Printer(); 
+     
+     //memory allocation for A	
+     int i,j;
+     A=(int**)malloc(L2*sizeof(int*)); 
+     for (i=0;i<L2; i++){ 
+         A[i]= (int*)malloc(C2*sizeof(int)); 
+     } 
+  	
+     //setting up the threads
+     pthread_t producers_thread[L1],consumer_thread[L1]; 
+  
+     //creating the threads
+     for (i=0;i<L2;i++){ 
+         int *row=malloc(sizeof(int)); 
+         *row=i; 
+         if(pthread_create(&producers_thread[i],NULL,producer,row)!=0){ 
+             perror("\n Thread creation error !\n"); 
+             exit(EXIT_FAILURE); 
+         } 
+         if(pthread_create(&consumer_thread[i],NULL,consumer,row)!=0){ 
+             perror("\n Thread creation error !\n"); 
+             exit(EXIT_FAILURE); 
+         } 
+     } 
+     
+     //waiting for the threads
+     for(i=0;i<L1;i++){ 
+         pthread_join(producers_thread[i],NULL); 
+     } 
+     for(i=0;i<L1;i++){ 
+         pthread_join(consumer_thread[i],NULL); 
+     } 
+  
+     //print Matrix C
+     printf("\nTHE RESULT (Matrix A):\n"); 
+     for(i=0;i<C2;i++){
+         for(j=0;j<L2;j++){ 
+             printf("%d\t",A[i][j]); 
+         } 
+         printf("\n"); 
+     } 
+     printf("\n"); 
 
-//attente des threads
+     //free the allocated memory
+     Matrices_Destroyer();
 
-
-
-
-//destruction...
-
-
-
-return 0;
-}
+     pthread_mutex_destroy(&mutex); 
+     sem_destroy(&empty); 
+     sem_destroy(&full); 
+  
+     return 0; 
+  }
