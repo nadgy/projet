994b074b496d1d95a1c7beb05c58c88520ac1e48
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-12-31 13:51:00 +0100

    amina

--- matrice.c
+++ matrice.c
@@ -1,70 +1,134 @@
-//section include..
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
+#include <ctype.h> // Inclure le fichier d'en-tête ctype.h
 
+#define N 5 // Définir la taille des matrices
 
+int B[N][N];
+int C[N][N];
+int A[N][N];
+int T[N * N]; // Tableau pour stocker les données produites
+pthread_mutex_t mutex; // Mutex pour la section critique
+sem_t empty; // Sémaphore pour les cases vides
+sem_t full; // Sémaphore pour les cases pleines
 
-//define
-#define N ... // places dans le buffer
+int produce() {
+    return rand() % 10;
+}
 
-//variable globales 
-//les matrices
-B,C,A
-//le tampon
-T
-
-//pour la synchronisation 
-pthread_mutex_t mutex;
-sem_t empty;
-sem_t full;
-
-// Producteur
-void producer(void)
-{
-  int item;
-//pour chaque ligne 
-//for....
-  {
-    item=produce(item);
-    sem_wait(&empty); // attente d'une place libre
+void insert_item(int item) {
+    static int index = 0;
     pthread_mutex_lock(&mutex);
-     // section critique
-     insert_item();
+    T[index] = item;
+    index = (index + 1) % (N * N); // Utilisation du modulo pour un tampon circulaire
     pthread_mutex_unlock(&mutex);
-    sem_post(&full); // il y a une place remplie en plus
-  }
 }
 
-void consumer(void)
-{
- int item;
- while(true)
- {
-   sem_wait(&full); // attente d'une place remplie
-   pthread_mutex_lock(&mutex);
-    // section critique
-    item=remove(item);
-   pthread_mutex_unlock(&mutex);
-   sem_post(&empty); // il y a une place libre en plus
- }
+int remove_item() {
+    static int index = 0;
+    int item;
+    pthread_mutex_lock(&mutex);
+    item = T[index];
+    index = (index + 1) % (N * N); // Utilisation du modulo pour un tampon circulaire
+    pthread_mutex_unlock(&mutex);
+    return item;
 }
 
-int Main ()
-{
-// Initialisation
-sem_init(&mutex,0,1);//exclusion mutuelle 
-sem_init(&empty, 0 , N);  // buffer vide
-sem_init(&full, 0 , 0);   // buffer vide
-//creation des threads
+void *producer(void *arg) {
+    int nb = *((int *)arg);
+    for (int i = 0; i < nb; i++) {
+        int item = produce();
+        sem_wait(&empty); // Attendre qu'il y ait une case vide
+        insert_item(item); // Insérer l'item dans le tableau
+        sem_post(&full); // Signaler qu'il y a une case pleine
+    }
+    pthread_exit(NULL);
+}
 
+void *consumer(void *arg) {
+    int nb = *((int *)arg);
+    while (nb > 0) {
+        sem_wait(&full); // Attendre qu'il y ait une case pleine
+        int item = remove_item(); // Retirer l'item du tableau
+        sem_post(&empty); // Signaler qu'il y a une case vide
+        nb--;
+    }
+    pthread_exit(NULL);
+}
 
+int main() {
+    sem_init(&empty, 0, N * N); // Initialiser le sémaphore empty à la taille maximale du tampon
+    sem_init(&full, 0, 0); // Initialiser le sémaphore full à 0
+    pthread_mutex_init(&mutex, NULL); // Initialiser le mutex
+    srand(time(NULL));
 
-//attente des threads
+    // Initialiser les matrices B et C aléatoirement
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            B[i][j] = rand() % 10;
+            C[i][j] = rand() % 10;
+            A[i][j] = 0;
+        }
+    }
 
+    pthread_t threads_prod[N];
+    pthread_t threads_cons[N];
+    int nb = N; // Nombre d'itérations pour chaque thread
+    int i; // Déclarer la variable i
+    for (i = 0; i < N; i++) {
+        if (pthread_create(&threads_prod[i], NULL, producer, &nb) != 0 ||
+            pthread_create(&threads_cons[i], NULL, consumer, &nb) != 0) {
+            perror("pthread_create");
+            exit(EXIT_FAILURE);
+        }
+    }
 
+    for (i = 0; i < N; i++) {
+        if (pthread_join(threads_prod[i], NULL) != 0 ||
+            pthread_join(threads_cons[i], NULL) != 0) {
+            perror("pthread_join");
+            exit(EXIT_FAILURE);
+        }
+    }
 
+    sem_destroy(&empty); // Détruire le sémaphore empty
+    sem_destroy(&full); // Détruire le sémaphore full
+    pthread_mutex_destroy(&mutex); // Détruire le mutex
 
-//destruction...
+    // Copier les éléments de T dans la matrice A
+    for (i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            A[i][j] = T[i * N + j];
+        }
+    }
 
+    // Affichage des matrices B, C et A
+    printf("Matrice B :\n");
+    for (i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", B[i][j]);
+        }
+        printf("\n");
+    }
 
+    printf("\nMatrice C :\n");
+    for (i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
 
-return 0;
+    printf("\nMatrice A :\n");
+    for (i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", A[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
